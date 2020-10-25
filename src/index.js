3c9d4d9dcfbcc19dc44e820afa424fa9455dabb3
levon72 <stas020699@mail.ru>
2020-10-25 17:20:45 +0300

    Update index.js
    test success

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+
+    n = String(Math.abs(n));
+    n = n.split('');
+    let res = '';
+  for (i=0;i<n.length;i++){
+      res = res + n[n.length - i - 1];
+  }
+  return Number(res);
 }
